0f4d51d6a81d92e7254b71c078c866666a0d7b10
Roman Madyanov <romantaken@gmail.com>
2018-08-24 21:21:14 +0300

    + testing environment

---
+++ www/wait_for_services.php
@@ -0,0 +1,49 @@
+<?php
+
+$mysqlReady = false;
+$memcachedReady = false;
+$redisReady = false;
+
+$memcached = new Memcached();
+$redis = new Redis();
+
+for ($i = 0; $i < 30; ++$i) {
+    if (!$mysqlReady) {
+        try {
+            $mysql = new PDO(
+                'mysql:host=' . getenv('MYSQL_HOST') . ';port=' . getenv('MYSQL_PORT') . ';dbname=' . getenv('MYSQL_DATABASE'),
+                getenv('MYSQL_USER'),
+                getenv('MYSQL_PASSWORD')
+            );
+
+            $mysqlReady = true;
+        } catch (Exception $e) {
+            $mysqlReady = false;
+        }
+    }
+
+    if (!$memcachedReady) {
+        try {
+            $memcachedReady = $memcached->addServer(getenv('MEMCACHED_HOST'), getenv('MEMCACHED_PORT'));
+        } catch (Exception $e) {
+            $memcachedReady = false;
+        }
+    }
+
+    if (!$redisReady) {
+        try {
+            $redis->connect(getenv('REDIS_HOST'), getenv('REDIS_PORT'), 1);
+            $redisReady = $redis->ping() === '+PONG';
+        } catch (Exception $e) {
+            $redisReady = false;
+        }
+    }
+
+    if ($mysqlReady && $memcachedReady && $redisReady) {
+        exit(0);
+    }
+
+    sleep(1);
+}
+
+exit(1);

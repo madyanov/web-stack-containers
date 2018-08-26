2247fd20d4731f759200f4d610062c3e09a62ca2
Roman Madyanov <romantaken@gmail.com>
2018-08-26 12:32:01 +0300

    * fix memcached container

--- www/wait_for_services.php
+++ www/wait_for_services.php
@@ -42,6 +42,18 @@ for ($i = 0; $i < 30; ++$i) {
 
     if ($mysqlReady && $memcachedReady && $redisReady) {
         exit(0);
+    } else {
+        if (!$mysqlReady) {
+            echo 'MySQL not ready (' . getenv('MYSQL_HOST') . ':' . getenv('MYSQL_PORT') . ")\n";
+        }
+
+        if (!$memcachedReady) {
+            echo 'Memcached not ready (' . getenv('MEMCACHED_HOST') . ':' . getenv('MEMCACHED_PORT') . ")\n";
+        }
+
+        if (!$redisReady) {
+            echo 'Redis not ready (' . getenv('REDIS_HOST') . ':' . getenv('REDIS_PORT') . ")\n";
+        }
     }
 
     sleep(1);
